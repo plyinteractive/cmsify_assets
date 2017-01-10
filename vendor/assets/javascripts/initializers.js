57cd56617d84f24d6f78b23c59c8b49bd2a8aa60
Andrew Shenstone <andrew@plyinteractive.com>
2017-01-09 16:01:47 -0800

    add comment about missing gems

--- vendor/assets/javascripts/initializers.js
+++ vendor/assets/javascripts/initializers.js
@@ -1,4 +1,7 @@
 Dropzone.autoDiscover = false;
+// TODO: Initializers depend on libraries loaded through rails-assets gems.
+// Gems can't be loaded through alternate sources (rails-assets.org) in .gemspec, so the best alternative
+// will be adding bower to the gemspec somehow
 $(document).on('turbolinks:load', function() {
   UIkit.init();
   Cmsify.initTinymce();
