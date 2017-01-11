223c59893f1e830724d35899086bbc2ccb456c2b
Andrew Shenstone <shenst1@gmail.com>
2017-01-11 14:01:00 -0800

    Scope the one button to the instance of js-dropzone.

--- vendor/assets/javascripts/initializers.js
+++ vendor/assets/javascripts/initializers.js
@@ -39,13 +39,14 @@ $(document).on('turbolinks:load', function() {
     });
   });
   $('.js-dropzone').each(function() {
-    $(this).dropzone({
+    var $doneButton = $(this).find('.js-dropzone-done');
+    $(this).find('form').dropzone({
       dictDefaultMessage: 'Drop files or click here to upload.',
       init: function() {
         this.on("addedfile", function(files) {
-          $('.js-dropzone-done').addClass('disabled');
+          $doneButton.addClass('disabled');
         }).on("queuecomplete", function(files) {
-          $('.js-dropzone-done').removeClass('disabled');
+          $doneButton.removeClass('disabled');
         });
       }
     });
