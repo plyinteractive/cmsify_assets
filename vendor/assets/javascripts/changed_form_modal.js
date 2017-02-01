f69302025fdd326eec0387eb648fa4195af7c73d
toastynerd <tylermorgan86@gmail.com>
2017-02-01 14:03:59 -0800

    use class instead of id for modal

--- vendor/assets/javascripts/changed_form_modal.js
+++ vendor/assets/javascripts/changed_form_modal.js
@@ -2,7 +2,7 @@ var NR = Cmsify.NestedResource.prototype;
 var CFM = Cmsify.ChangedFormModal = function(el) {
   this.$el = $(el);
   this.form = this.$el.find('form');
-  this.modal = UIkit.modal(this.$el.find('#js-unsaved-modal'));
+  this.modal = UIkit.modal(this.$el.find('.js-unsaved-modal'));
   this.$el.find('.js-check-changed').each(function(index, ele) {
     $(ele).on('click', function(e) {
       if (this.setIsModified()) {
