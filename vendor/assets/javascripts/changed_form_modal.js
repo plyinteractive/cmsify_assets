15e32a42a1b64e5fcd9e6d9ea93643c72038c6ef
toastynerd <tylermorgan86@gmail.com>
2017-03-01 11:45:35 -0800

    bug fix and slight cleanup

--- vendor/assets/javascripts/changed_form_modal.js
+++ vendor/assets/javascripts/changed_form_modal.js
@@ -2,30 +2,43 @@ var NR = Cmsify.NestedResource.prototype;
 var filterAuthenticityToken = Cmsify.util.filterAuthenticityToken;
 var CFM = Cmsify.ChangedFormModal = function(el) {
   this.$el = $('form');
-  if(!this.$el.length);
+  if(!this.$el.length) return;
   this.lastData = this.serializeFields();  
   $('a').on('click', function(event) {
-    if ((typeof(event.target.dataset['ukModal']) !== 'undefined') || $(event.target).hasClass('uk-modal-close')) return;
-    if (this.setIsModified()) {
-      this.modalOpened = true;
-      event.preventDefault();
-      var $unsavedModal = UIkit.modal('.js-unsaved-modal');
-      $unsavedModal.show();
-      $unsavedModal.find('.js-modal-confirm').on('click', function() {
-        window.location.href = $(event.target).attr('href');
-      });
-      $unsavedModal.on('hide.uk.modal', function() {
-        this.modalOpened = false;
-      }.bind(this));
-    }
+    if (this.isInternalLink(event.target)) return;
+    if (this.setIsModified()) this.openWarningModal();
   }.bind(this));
+
   window.onbeforeunload = function(event) {
-    if (this.setIsModified() && !this.modalOpened) return "You have unsaved changes, are you sure?";
-    modalOpened = false;
+    if (this.setIsModified() && !this.isWarningModalOpened) return "You have unsaved changes, are you sure?";
+    this.isWarningModalOpened = false;
   }.bind(this);
 }
 
 CFM.prototype.serializeFields = function() {
   return filterAuthenticityToken(NR.serializeFields.call(this));
 } 
+
 CFM.prototype.setIsModified = NR.setIsModified
+
+CFM.prototype.openWarningModal = function() {
+  this.isWarningModalOpened = true;
+  event.preventDefault();
+  var $unsavedModal = UIkit.modal('.js-unsaved-modal');
+  $unsavedModal.show();
+  $unsavedModal.find('.js-modal-confirm').on('click', function() {
+    window.location.href = $(event.target).attr('href');
+  });
+  $unsavedModal.on('hide.uk.modal', function() {
+    this.isWarningModalOpened = false;
+  }.bind(this));
+}
+
+CFM.prototype.isInternalLink = function(target) {
+  var $target = $(target);
+  return (
+    (typeof(target.data('ukModal')) !== 'undefined') || 
+    $target.hasClass('uk-modal-close') || 
+    $target.hasClass('js-remove-image')
+  )
+};
