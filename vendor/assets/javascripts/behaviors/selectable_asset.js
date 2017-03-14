b14729c2207323bdf4b9b42c94e60819a3bfe285
Josh Reeves <josh@plyinteractive.com>
2017-03-14 15:03:59 -0700

    Show small preview image or file icon

--- vendor/assets/javascripts/behaviors/selectable_asset.js
+++ vendor/assets/javascripts/behaviors/selectable_asset.js
@@ -6,12 +6,40 @@ Cmsify.SelectableAsset = function(el) {
 };
 
 Cmsify.SelectableAsset.prototype.select = function() {
-  $('.js-' + this.$el.data('asset-name')).html(this.renderImageOrFileName());
+  $preview = $('.js-' + this.$el.data('asset-name'));
+  $preview.html(this.renderAssetOriginalPreview());
+  $preview.append($('<p />').html(this.getFileName()));
   $('.js-remove-asset').removeClass('uk-hidden');
 };
 
-Cmsify.SelectableAsset.prototype.renderImageOrFileName = function() {
+Cmsify.SelectableAsset.prototype.renderAssetOriginalPreview = function() {
+  return this.renderAssetPreview(this.getUrl());
+};
+
+Cmsify.SelectableAsset.prototype.renderAssetIconPreview = function() {
+  return this.renderAssetPreview(this.getIconUrl());
+};
+
+Cmsify.SelectableAsset.prototype.renderAssetPreview = function(imageUrl) {
   return this.$el.data('asset-type').includes('image') ?
-    $('<img class="cmsify-width-medium " src="' + this.$el.data('asset-url') + '" />') :
-    this.$el.data('asset-url').split('/').pop();
+    $('<img class="cmsify-width-medium " src="' + imageUrl + '" />') :
+    $('<i class="uk-icon-file-o uk-icon-large uk-text-muted"></i>');
+};
+
+Cmsify.SelectableAsset.prototype.getFilePath = function() {
+  fileName = this.$el.data('asset-url').split('/');
+  fileName.pop();
+  return fileName.join('/');
+};
+
+Cmsify.SelectableAsset.prototype.getFileName = function() {
+  return this.$el.data('asset-url').split('/').pop();
+};
+
+Cmsify.SelectableAsset.prototype.getUrl = function() {
+  return this.$el.data('asset-url');
+};
+
+Cmsify.SelectableAsset.prototype.getIconUrl = function() {
+  return this.getFilePath() + "/icon_" + this.getFileName();
 };
