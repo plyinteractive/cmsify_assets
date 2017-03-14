1caa9dd759529292b8387dcc3ab7bf3b552f7f75
Josh Reeves <josh@plyinteractive.com>
2017-03-14 13:17:04 -0700

    Dry up asset uploads so we get some stuff for freeeeee

--- lib/cmsify_assets/version.rb
+++ lib/cmsify_assets/version.rb
@@ -1,3 +1,3 @@
 module CmsifyAssets
-  VERSION = "0.1.0"
+  VERSION = "0.1.1"
 end
--- vendor/assets/javascripts/behaviors/selectable_rows.js
+++ vendor/assets/javascripts/behaviors/selectable_rows.js
@@ -2,13 +2,8 @@ Cmsify.SelectableRows = function(el) {
   this.$el = $(el);
   this.$el.find('tr').each(function() {
     var $this = $(this);
-    var $inputs = $this.find(':radio, :checkbox');
     $this.on('click', function(e) {
-      e.preventDefault();
-      $inputs.trigger('click');
-    });
-    $inputs.on('click', function(e) {
-      e.stopPropagation();
+      $this.find(':radio, :checkbox').prop('checked', true);
     });
   });
 };
