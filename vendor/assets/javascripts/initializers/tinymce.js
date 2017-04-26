63665e53315a9a064543267c092b0e0510865850
Josh Reeves <josh@plyinteractive.com>
2017-04-26 11:27:01 -0700

    Fix image asset root

--- vendor/assets/javascripts/initializers/tinymce.js
+++ vendor/assets/javascripts/initializers/tinymce.js
@@ -1,4 +1,5 @@
 $(document).on('cmsify:load', function() {
+  var cmsifyRoot = window.location.pathname.split("/")[1];
   tinymce.remove();
   tinymce.init(
     {
@@ -6,7 +7,8 @@ $(document).on('cmsify:load', function() {
       height: 250,
       plugins: "searchreplace code fullscreen media paste code lists advlist image",
       toolbar: "code image bullist numlist",
-      image_list: "/admin/assets.json",
+      relative_urls: false,
+      image_list: "/" + cmsifyRoot + "/assets.json",
       setup: function(editor) {
         editor.on('change', function(e) {
           $('input[id='+ e.target.id + ']').trigger('tinymce:change');
