2fcfb8776c315dadaab42a46c0d288e02191c6a0
Andrew Shenstone <shenst1@gmail.com>
2018-06-14 11:45:07 -0700

    Overwrite jquery contains method to not care about case sensitivity.

--- vendor/assets/javascripts/initializers/form_filters.js
+++ vendor/assets/javascripts/initializers/form_filters.js
@@ -13,6 +13,18 @@ $(document).on('cmsify:load', function() {
       $(this).closest('form').submit();
     });
   });
+  // Overwrite the contains method to not be case sensitive
+  // NEW selector
+  jQuery.expr[':'].Contains = function(a, i, m) {
+    return jQuery(a).text().toUpperCase()
+        .indexOf(m[3].toUpperCase()) >= 0;
+  };
+
+  // OVERWRITES old selecor
+  jQuery.expr[':'].contains = function(a, i, m) {
+    return jQuery(a).text().toUpperCase()
+        .indexOf(m[3].toUpperCase()) >= 0;
+  };
 
   $('[data-filter-search]').each(function() {
     var $this = $(this);
